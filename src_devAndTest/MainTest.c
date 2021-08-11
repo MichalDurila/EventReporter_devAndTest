8882d75442237248789ebee25d724db2f9286ac4
Michal Durila <durilmic@fit.cvut.cz>
2021-08-11 23:30:18 +0200

    Added a test for incrementing of the event counters

--- src_devAndTest/MainTest.c
+++ src_devAndTest/MainTest.c
@@ -25,10 +25,13 @@
 #include "../src/Storage.h"
 #include "../src/EventHandler.h"
 
-void TC_01(void);
+void MainTest_TC_01(void);
+void MainTest_OutputCounters(void);
 
 int main(void) {
     uint8_t u8TestVariable = 0U;
+
+    EventHandler_InitializeOnStart();
 
     printf("main: start\n");
     NvmMem_Write(0x00001000, &u8TestVariable, 2U);
@@ -36,20 +39,30 @@ int main(void) {
     Storage_StoreEventReport(&u8TestVariable, 2U);
     printf("main: end\n\n");
 
-    TC_01();
+    MainTest_TC_01();
 
     return 0;
 }
 
-void TC_01(void)
+void MainTest_TC_01(void)
 {
     uint8_t u8TestVariable = 0U;
 
     printf("TC_01: start\n");
+    MainTest_OutputCounters();
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
+    MainTest_OutputCounters();
     NvmMem_Write(0x00001000, NULL, 2U);
+    MainTest_OutputCounters();
     NvmMem_Write(0x00001000, &u8TestVariable, 0U);
+    MainTest_OutputCounters();
     printf("TC_01: end\n\n");
 
+    return;
+}
+
+void MainTest_OutputCounters(void)
+{
+    printf("Counter address = %d, counter NULL = %d, counter size = %d\n", EventHandler_GetEventsCounter(E_EVENTHANDLER_SEVERITY_NORMAL, E_EVENTHANDLER_TYPE_ADDRESSRANGE), EventHandler_GetEventsCounter(E_EVENTHANDLER_SEVERITY_MEDIUM, E_EVENTHANDLER_TYPE_NULLARGUMENT), EventHandler_GetEventsCounter(E_EVENTHANDLER_SEVERITY_LOW, E_EVENTHANDLER_TYPE_MINDATALENGTH));
     return;
 }
