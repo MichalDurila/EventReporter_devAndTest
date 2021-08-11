f7efb45d0971d8a57a00c5c9cc2ffee0d42dab6f
Michal Durila <durilmic@fit.cvut.cz>
2021-08-11 19:55:23 +0200

    Added a test case for testing a basic functionality of the reporting function

--- src_devAndTest/MainTest.c
+++ src_devAndTest/MainTest.c
@@ -23,6 +23,9 @@
 #include "../src/Comm.h"
 #include "../src/NvmMem.h"
 #include "../src/Storage.h"
+#include "../src/EventHandler.h"
+
+void TC_01(void);
 
 int main(void) {
     uint8_t u8TestVariable = 0U;
@@ -33,5 +36,20 @@ int main(void) {
     Storage_StoreEventReport(&u8TestVariable, 2U);
     printf("main: end\n\n");
 
+    TC_01();
+
     return 0;
+}
+
+void TC_01(void)
+{
+    uint8_t u8TestVariable = 0U;
+
+    printf("TC_01: start\n");
+    NvmMem_Write(0x00000000, &u8TestVariable, 2U);
+    NvmMem_Write(0x00001000, NULL, 2U);
+    NvmMem_Write(0x00001000, &u8TestVariable, 0U);
+    printf("TC_01: end\n\n");
+
+    return;
 }
