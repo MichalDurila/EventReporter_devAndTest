ffce6991ed8222e2c01e1c3fee50ab4445f2ca42
Michal Durila <durilmic@fit.cvut.cz>
2021-08-13 02:16:19 +0200

    Tested the reporting functionality * Tidied up the code Added some comments

--- src_devAndTest/MainTest.c
+++ src_devAndTest/MainTest.c
@@ -20,9 +20,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../src/Comm.h"
 #include "../src/NvmMem.h"
-#include "../src/Storage.h"
 #include "../src/EventHandler.h"
 #include "../src/Timing.h"
 
@@ -32,22 +30,15 @@ static const float64_t m_f64TimeIncrement = 20.0;
 static void MainTest_OutputCounters(void);
 static void MainTest_OutputStandbyModes(void);
 static void MainTest_OutputEnabledReporting(void);
+static void MainTest_OutputStatistics(void);
 static void MainTest_TC_01(void);
 static void MainTest_TC_02(void);
 static void MainTest_TC_03(void);
 
 
 int main(void) {
-    uint8_t u8TestVariable = 0U;
-
     EventHandler_InitializeOnStart();
     Timing_SetTime(TIMING_INITIAL_TIME);
-
-    printf("main: start\n");
-    NvmMem_Write(0x00001000, &u8TestVariable, 2U);
-    Comm_SendEventReport(&u8TestVariable, 2U);
-    Storage_StoreEventReport(&u8TestVariable, 2U);
-    printf("main: end\n\n");
 
     MainTest_TC_01();
     MainTest_TC_02();
@@ -56,109 +47,98 @@ int main(void) {
     return 0;
 }
 
+/**
+ * @brief Tests 3 basic types of events
+ */
 static void MainTest_TC_01(void)
 {
-    uint8_t u8TestVariable = 0U;
+    uint8_t au8TestArray[10U];
 
     printf("TC_01: start\n");
-    MainTest_OutputCounters();
+    MainTest_OutputStatistics();
 
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
-    NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
+    NvmMem_Write(0x00000000, au8TestArray, 2U);
+    MainTest_OutputStatistics();
 
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
     NvmMem_Write(0x00001000, NULL, 2U);
-    MainTest_OutputCounters();
+    MainTest_OutputStatistics();
 
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
-    NvmMem_Write(0x00001000, &u8TestVariable, 0U);
-    MainTest_OutputCounters();
+    NvmMem_Write(0x00001000, au8TestArray, 0U);
+    MainTest_OutputStatistics();
 
     printf("TC_01: end\n\n");
-
     return;
 }
 
+/**
+ * @brief Tests the protection against overflow and re-enabling of reporting
+ */
 static void MainTest_TC_02(void)
 {
     uint8_t u8TestVariable = 0U;
 
     printf("TC_02: start\n");
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
+    MainTest_OutputStatistics();
 
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
+    MainTest_OutputStatistics();
 
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
+    MainTest_OutputStatistics();
 
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
+    MainTest_OutputStatistics();
 
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
+    MainTest_OutputStatistics();
 
     Timing_SetTime(Timing_GetTime() + 600.1);
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
+    MainTest_OutputStatistics();
 
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
+    MainTest_OutputStatistics();
 
     printf("TC_02: end\n\n");
-
     return;
 }
 
+/**
+ * @brief Tests disabling of event reporting
+ */
 static void MainTest_TC_03(void)
 {
     uint8_t u8TestVariable = 0U;
 
     printf("TC_03: start\n");
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
-    MainTest_OutputEnabledReporting();
+    MainTest_OutputStatistics();
 
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
-    MainTest_OutputEnabledReporting();
+    MainTest_OutputStatistics();
 
     EventHandler_SetEnabledReporting(E_EVENTHANDLER_TYPE_ADDRESSRANGE, E_FALSE);
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
-    MainTest_OutputEnabledReporting();
+    MainTest_OutputStatistics();
 
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
-    MainTest_OutputEnabledReporting();
+    MainTest_OutputStatistics();
 
     EventHandler_SetEnabledReporting(E_EVENTHANDLER_TYPE_ADDRESSRANGE, E_TRUE);
     Timing_SetTime(Timing_GetTime() + m_f64TimeIncrement);
     NvmMem_Write(0x00000000, &u8TestVariable, 2U);
-    MainTest_OutputCounters();
-    MainTest_OutputStandbyModes();
-    MainTest_OutputEnabledReporting();
+    MainTest_OutputStatistics();
 
     printf("TC_03: end\n\n");
-
     return;
 }
 
@@ -177,5 +157,13 @@ static void MainTest_OutputStandbyModes(void)
 static void MainTest_OutputEnabledReporting(void)
 {
     printf("Enabled Reporting %d, %d, %d, %d, %d\n", EventHandler_GetEnabledReporting(E_EVENTHANDLER_TYPE_NULLARGUMENT), EventHandler_GetEnabledReporting(E_EVENTHANDLER_TYPE_MINDATALENGTH), EventHandler_GetEnabledReporting(E_EVENTHANDLER_TYPE_ADDRESSRANGE), EventHandler_GetEnabledReporting(E_EVENTHANDLER_TYPE_DIVISIONBYZERO), EventHandler_GetEnabledReporting(E_EVENTHANDLER_TYPE_UNUPDATEDCONSTANTS));
+    return;
+}
+
+static void MainTest_OutputStatistics(void)
+{
+    MainTest_OutputCounters();
+    MainTest_OutputStandbyModes();
+    MainTest_OutputEnabledReporting();
     return;
 }
